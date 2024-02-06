21af4dfd240d8819cbd863e3038a863132815a7c
Xiaoxiao Lin <92532864+Roawaal@users.noreply.github.com>
2024-02-06 17:27:52 +0000

    Open http://127.0.0.1:8000/2022england

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask_cors import *
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -25,6 +26,10 @@ def contact():
 @app.route('/map')
 def show_map():
     return render_template('static/uk_regions_map.html')
+
+@app.route('/2022england')
+def index():
+    return render_template('2022england.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
