1959938bd49394843580a71d9942e5f537ed54c4
Xiaoxiao Lin <92532864+Roawaal@users.noreply.github.com>
2024-04-18 14:08:59 +0100

    Update

--- Brexit/app.py
+++ Brexit/app.py
@@ -8,5 +8,9 @@ def home():
     dynamic_content = "This is dynamic content"
     return render_template('home.html', content=dynamic_content)
 
+@app.route('/map')
+def chart():
+    return render_template('4_map.html')
+
 if __name__ == '__main__':
     app.run()
